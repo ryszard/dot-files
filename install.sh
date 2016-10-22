76fefa3643ccee4c3e54725eb61f148e521328a3
Ric Szopa <ryszard.szopa@gmail.com>
2016-10-23 00:18:44 +0200

    More changes in install script.

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ set -ex
 target=${target:-$HOME}
 cd config
 for file in *; do
-  ln -sf "$(pwd)/$file" $target/.$file
+  if [[ ! -e $target/.$file ]]; then
+	  ln -sf "$(pwd)/$file" $target/.$file
+  fi
 done
